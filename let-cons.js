bc53c4d2b0b8af29c7e2e10d0f2b90989a3f2bc2
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-03-01 12:21:13 +0600

    i fenidsed video 2

--- let-cons.js
+++ let-cons.js
@@ -1,3 +1,37 @@
-const name = 'Bangladesh';
-name = 'Singapor'
-console.log(name)
+const rootMan = "conected"
+// console.log(rootMan)
+
+// for(let i = 1; i < 10; i++){
+//     console.log(i);
+// }
+
+const countryName = "BD " + rootMan;
+
+// console.log(countryName);
+
+
+let price = 120; 
+
+price =150;
+price = 555;
+price = 999999;
+// console.log(price)
+
+// console.log(p)
+
+var p = 777; 
+
+
+// console.log(p)
+
+
+const student = {
+    name: 'Johan',
+
+    age: 22,
+
+}
+
+student.name = 'jane'
+student.age = 30;
+console.log(student);
